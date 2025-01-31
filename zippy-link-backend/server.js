37cf28681052289483d1c5bce38c3ec1bde93d00
Ibtissemkr <ibtissem.kraiem2@gmail.com>
2025-01-31 22:35:42 +0100

    Initialize project and basic Express server and connect backend to MongoDB

---
+++ zippy-link-backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db'); 
+const urlRoutes = require('./routes/urlRoutes'); 
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); 
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
